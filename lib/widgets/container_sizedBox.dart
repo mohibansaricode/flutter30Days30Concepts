9ac01364e4952e74a921802ec8dab4f8725318c4
mohibansaricode <mmcode46@gmail.com>
2025-07-02 23:23:52 +0500

    container And sizedBox

---
+++ lib/widgets/container_sizedBox.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ContainerSizedbox extends StatelessWidget {
+  const ContainerSizedbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Container Widget"),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 50, width: 100),
+            Container(width: 100, height: 100, color: Colors.amberAccent),
+            SizedBox(height: 50, width: 50),
+            Container(width: 100, height: 100, color: Colors.blueAccent),
+          ],
+        ),
+      ),
+    );
+  }
+}
