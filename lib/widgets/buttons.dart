84d6bfb07268ffc348059d0aa3956227bb85c27e
mohibansaricode <mmcode46@gmail.com>
2025-07-03 13:42:26 +0500

    elevated buttons

--- lib/widgets/buttons.dart
+++ lib/widgets/buttons.dart
@@ -11,20 +11,20 @@ class Buttons extends StatelessWidget {
         backgroundColor: Colors.deepPurple,
       ),
       body: Center(
-        child: TextButton(
-          style: ButtonStyle(
-            backgroundColor: WidgetStatePropertyAll(Colors.amberAccent),
-          ),
-          onPressed: () {},
-          child: Text("click me", style: TextStyle(fontSize: 20)),
-        ),
-        // child: ElevatedButton(
+        // child: TextButton(
         //   style: ButtonStyle(
-        //     backgroundColor: WidgetStatePropertyAll(Colors.white),
+        //     backgroundColor: WidgetStatePropertyAll(Colors.amberAccent),
         //   ),
         //   onPressed: () {},
-        //   child: Text("click me"),
+        //   child: Text("click me", style: TextStyle(fontSize: 20)),
         // ),
+        child: ElevatedButton(
+          style: ButtonStyle(
+            backgroundColor: WidgetStatePropertyAll(Colors.white),
+          ),
+          onPressed: () {},
+          child: Text("click me"),
+        ),
       ),
     );
   }
