984120938e1c1763e1c294d06e03e0d1da37eb56
mohibansaricode <mmcode46@gmail.com>
2025-07-03 13:41:23 +0500

    text buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/widgets/buttons.dart';
 import 'package:my_app/widgets/container_sizedBox.dart';
 import 'package:my_app/widgets/rows_colums.dart';
 
@@ -18,7 +19,8 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.deepPurple,
       ),
       // home: ContainerSizedbox(),
-      home: RowsColums(),
+      // home: RowsColums(),
+      home: Buttons(),
     );
   }
 }
