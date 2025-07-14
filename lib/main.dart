c94b3ffd908036fc35c8f70d76c6d0a3358b900f
mohibansaricode <mmcode46@gmail.com>
2025-07-14 21:01:40 +0500

    snack bar

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
       // home: ContainerSizedbox(),
       // home: RowsColums(),
       // home: Buttons(),
-      home: DismissibleWidget(),
+      home: BottomSnack(),
     );
   }
 }
--- lib/widgets/snack_bar.dart
+++ lib/widgets/snack_bar.dart
@@ -1,36 +1,36 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class BottomSnack extends StatefulWidget {
-//   const BottomSnack({super.key});
+class BottomSnack extends StatefulWidget {
+  const BottomSnack({super.key});
 
-//   @override
-//   State<BottomSnack> createState() => _SnackBarState();
-// }
+  @override
+  State<BottomSnack> createState() => _SnackBarState();
+}
 
-// class _SnackBarState extends State<BottomSnack> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text("Snack Bar"),
-//         backgroundColor: Colors.deepPurpleAccent,
-//       ),
-//       body: Center(
-//         child: ElevatedButton(
-//           onPressed: () {
-//             final snackBar = SnackBar(
-//               duration: const Duration(milliseconds: 1000),
-//               shape: RoundedRectangleBorder(
-//                 borderRadius: BorderRadius.circular(20),
-//               ),
-//               behavior: SnackBarBehavior.floating,
-//               content: Text("This is a snack bar"),
-//             );
-//             ScaffoldMessenger.of(context).showSnackBar(snackBar);
-//           },
-//           child: Text("Press me"),
-//         ),
-//       ),
-//     );
-//   }
-// }
+class _SnackBarState extends State<BottomSnack> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Snack Bar"),
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            final snackBar = SnackBar(
+              duration: const Duration(milliseconds: 1000),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              behavior: SnackBarBehavior.floating,
+              content: Text("This is a snack bar"),
+            );
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          },
+          child: Text("Press me"),
+        ),
+      ),
+    );
+  }
+}
